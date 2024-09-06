ee5f847a1e5fd8a5ed3a597d9648e1fc6634d18b
sanjay5656 <sanjaysanjay23ai@gmail.com>
2024-09-06 09:13:10 +0530

    san

--- dfs.py
+++ dfs.py
@@ -3,21 +3,26 @@
 '''Depth First Search uses STACK AND RECURSION
 '''
 #import defaultdict
+from collections import deque
 from collections import defaultdict
+
 def dfs(graph,start,visited,path):
     path.append(start)
     visited[start]=True
     for neighbour in graph[start]:
-       #type ur code here
+        if visited[neighbour]==False:
+            dfs(graph,neighbour,visited,path)
+            visited[neighbour]=True
     return path
 graph=defaultdict(list)
 n,e=map(int,input().split())
 for i in range(e):
-   #type ur code here
+    u,v=map(str,input().split())
+    graph[u].append(v)
     graph[v].append(u)
 #print(graph)
 start='A'
 visited=defaultdict(bool)
- #type ur code here
+path=[]
 traversedpath=dfs(graph,start,visited,path)
 print(traversedpath)
